32d39f52a1a3fdba27dfcbb5b2917157f224f728
JoasGomes <jojoca.vitor777@gmail.com>
2023-01-19 10:24:41 -0300

    Início do projeto

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { PomodoroTimer } from "./components/pomodoro-timer";
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <PomodoroTimer defaultPomodoroTime={1500} />
+    </div>
+  );
+}
+
+export default App;
